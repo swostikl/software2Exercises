3185dc4b9b2a37dc7b345adb4fa542632a4c8c42
Swostika lama <swostikl@metropolia.fi>
2024-11-16 19:57:25 +0200

    initial commit

--- exercise13.py
+++ exercise13.py
@@ -4,6 +4,8 @@
 # http://127.0.0.1:5000/prime_number/31. The response must be in the format of {"Number":31, "isPrime":true}.
 
 import math
+
+import mysql.connector
 from flask import Flask, request, jsonify
 app=Flask(__name__)
 
@@ -40,5 +42,35 @@ if __name__ == '__main__':
 # for EFHK would be: http://127.0.0.1:5000/airport/EFHK. The response must be in the format of: {"ICAO":"EFHK",
 # "Name":"Helsinki-Vantaa Airport", "Location":"Helsinki"}.
 
-from flask import Flask, request, jsonify
 
+connection = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    database='flight_game',
+    user='swosti',
+    password='Amazol12',
+    autocommit=True,
+    charset="utf8mb4",
+    collation="utf8mb4_general_ci"
+    )
+cursor=connection.cursor()
+
+@app.route('/airport/<ICAO>', methods=['GET'])
+def airport(ICAO):
+    ICAO = ICAO.upper()
+    sql="Select name, municipality from airport where ident=%s"
+    cursor.execute(sql, (ICAO,))
+    results = cursor.fetchall()
+    if len(results) !=1:
+        return jsonify({
+            "error": 'No airports available',
+            "code" : 400
+            }),400
+    else:
+            return jsonify({
+            "ICAO": ICAO,
+            "Name": results[0][0],
+            "location(Municipality)": results[0][1],
+        }),200
+
+app.run(host='127.0.0.1', port=5000)
